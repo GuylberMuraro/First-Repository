367d7d0c8c8696b47385d7478eb9ac039930dec1
Guylber Henrique Muraro <kodiakness@gmail.com>
2026-08-09 14:28:25 -0300

    errors fixed

--- dynamical-systems/the-game-of-life/main.py
+++ dynamical-systems/the-game-of-life/main.py
@@ -5,24 +5,36 @@ N = 8
 array = np.random.randint(0,2, size=(N,N))
 
 #Functions
-def neighbors(array):
+def neighbors(array): #counting neighbers
     kn = array.copy() # neighbors number
+    array1 = array.copy()
     n = len(array)
     for i in range(n):
         for j in range(n):
             #counting neighbors
-            x = array[(i+1)%n,j] + array[(i-1)%n,j]
-            y = array[i,(j+1)%n] + array[i,(j-1)%n]
-            p = array[(i-1)%n,(j+1)%n] + array[(i+1)%n,(j-1)%n]
-            s = array[(i-1)%n,(j-1)%n] + array[(i+1)%n,(j+1)%n]
+            x = array1[(i+1)%n,j] + array1[(i-1)%n,j]
+            y = array1[i,(j+1)%n] + array1[i,(j-1)%n]
+            p = array1[(i-1)%n,(j+1)%n] + array1[(i+1)%n,(j-1)%n]
+            s = array1[(i-1)%n,(j-1)%n] + array1[(i+1)%n,(j+1)%n]
 
             kn[i,j] = x + y + p + s
 
-            #applying the rules
-            if array[i,j] == 1 and (kn == 2 or kn == 3):
+            #applying the rules and update the array
+            if array1[i,j] == 1 and (kn[i,j] == 2 or kn[i,j] == 3): #[alive] --> [alive]
                 array[i,j] = 1
-            if array[i,j] == 0 and kn == 3:
+            if array1[i,j] == 0 and kn[i,j] == 3:                   #[dead] --> [alive]
                 array[i,j] = 1
-            else:
+            else:                                                   #[dead] --> [dead]
                 array[i,j] = 0
+    return kn
+            
+            
 
+#Simulation
+
+print(f'{array}\n\n')
+
+neighbors(array)
+
+
+print(array)
