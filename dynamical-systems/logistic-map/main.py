d1f4fd05596f0452914824ec4d5186809aed0f5a
Guylber Henrique Muraro <kodiakness@gmail.com>
2026-08-25 11:35:04 -0300

    ...

--- dynamical-systems/logistic-map/main.py
+++ dynamical-systems/logistic-map/main.py
@@ -8,15 +8,27 @@ r_list = np.arange(0, N1 + dt, dt)
 rs = len(r_list)
 x = np.zeros(N2+1)
 X = []
+R = []
 
 
 for i,r in enumerate(r_list):
     x[0] = 0.5
     for j in range(N2):
         x[j+1] = r * x[j] * (1 - x[j])
-    X.append(x[-10:])
+    c = (x[-10:].copy()).flatten()
+    X.append(c)
+    ri = np.tile(r,10)
+    R.append(ri)
 
-print(X)
+Xrr = np.array(X)
+Rrr = np.array(R)
+
+#Xf = Xrr.flatten()
+#Rf = Rrr.flatten()
+
+print(Xrr)
+#plt.scatter(Rf,Xf)
+#plt.show()
 '''
 R_list = r_list[-10:]
 # keep trying implementing tile method
