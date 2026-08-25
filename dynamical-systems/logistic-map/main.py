f721e5362d45a7bc6bb68d57508ce18878818a66
Guylber Henrique Muraro <kodiakness@gmail.com>
2026-08-25 11:41:17 -0300

    Logistic Map completed

--- dynamical-systems/logistic-map/main.py
+++ dynamical-systems/logistic-map/main.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Parameters
 N1 = 4
 N2 = 1000
 dt = 0.01
 r_list = np.arange(0, N1 + dt, dt)
-rs = len(r_list)
 x = np.zeros(N2+1)
 X = []
 R = []
 
-
+#Simulation
 for i,r in enumerate(r_list):
     x[0] = 0.5
     for j in range(N2):
@@ -20,23 +20,13 @@ for i,r in enumerate(r_list):
     ri = np.tile(r,10)
     R.append(ri)
 
+#Data Adjust
 Xrr = np.array(X)
 Rrr = np.array(R)
 
-#Xf = Xrr.flatten()
-#Rf = Rrr.flatten()
+Xf = Xrr.flatten()
+Rf = Rrr.flatten()
 
-print(Xrr)
-#plt.scatter(Rf,Xf)
-#plt.show()
-'''
-R_list = r_list[-10:]
-# keep trying implementing tile method
-Y.tile(R_list,(rs,1))
-
-print(Y)
-
-
-plt.scatter(RM, X)
+#Export
+plt.scatter(Rf, Xf, s=2)
 plt.show()
-'''
